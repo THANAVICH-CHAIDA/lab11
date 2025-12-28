ec26f308a1cd814d2598754bbf429e4a5186e36e
Thanavich <thanavich_c@cmu.ac.th>
2025-12-28 13:35:22 +0700

    lab11_1.cpp completed

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,40 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+using namespace std;
+
+
+int main(){
+    string count;
+    string grade;
+    string space;
+    cout << "Press Enter 3 times to reveal your future.";
+    for(int i = 1;i<=3;i++){
+        getline(cin,space);
+    }
+    
+        srand(time(0));
+        
+        int x = rand()%9 +1;
+        
+        if(x == 1) grade = "A";
+        
+        if(x == 2) grade = "B+";
+        
+        if(x == 3) grade = "B";
+        
+        if(x == 4) grade = "C+";
+        
+        if(x == 5) grade = "C";
+        
+        if(x == 6) grade = "D+";
+        
+        if(x == 7) grade = "D";
+        
+        if(x == 8) grade = "F";
+        
+        if(x == 9) grade = "W";
+        
+        cout << "You will get "<< grade << " in this 261102.";
+}
